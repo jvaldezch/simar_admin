da9841e701d9c833e98e16bf34bebb83ce022ce2
unknown <jvaldezch@gmail.com>
2018-07-02 18:32:33 -0500

    Update commands

--- application/modules/admin/controllers/GetController.php
+++ application/modules/admin/controllers/GetController.php
@@ -13,9 +13,9 @@ class Admin_GetController extends Zend_Controller_Action {
 
     public function preDispatch() {
         parent::preDispatch();
-        if (!$this->getRequest()->isXmlHttpRequest()) {
+        /*if (!$this->getRequest()->isXmlHttpRequest()) {
             throw new Zend_Controller_Request_Exception("Not an AJAX request detected");
-        }
+        }*/
         $auth = new Auth_Sessions();
         if ($auth->isAuthenticated()) {
                 $auth->actualizar();
@@ -471,6 +471,32 @@ class Admin_GetController extends Zend_Controller_Action {
         } catch (Exception $ex) {
             $this->_helper->json(array("success" => false, "message" => $ex->getMessage()));
         }
+    }
+
+    public function commandAction() {
+        ob_implicit_flush(true);
+        ob_end_flush();
+
+        $cmd = "/home/monitoreo_marino/repositories/git/pysimar/pysimar/./conversion.py --sensor ghrsst --composition m-nsst --type kmz --year 2018 --month 6";
+        echo $cmd;
+
+        $descriptorspec = array(
+        0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+        1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+        2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+        );
+
+
+        $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+
+        if (is_resource($process)) {
+
+            while ($s = fgets($pipes[1])) {
+                echo $s;
+
+            }
+        }
+
     }
 
     public function obtenerNsstAction() {
