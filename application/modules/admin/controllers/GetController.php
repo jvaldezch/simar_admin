479034539c8b92f4129245c79d8730c40c4adb36
unknown <jvaldezch@gmail.com>
2018-07-02 18:37:36 -0500

    Update commands

--- application/modules/admin/controllers/GetController.php
+++ application/modules/admin/controllers/GetController.php
@@ -474,26 +474,25 @@ class Admin_GetController extends Zend_Controller_Action {
     }
 
     public function commandAction() {
+
         ob_implicit_flush(true);
         ob_end_flush();
 
-        $cmd = "/home/monitoreo_marino/repositories/git/pysimar/pysimar/./conversion.py --sensor ghrsst --composition m-nsst --type kmz --year 2018 --month 6";
-        echo $cmd;
+        $cmd = $this->_appConfig->getParam("pysimar_home") . "/./conversion.py --sensor ghrsst --composition m-nsst --type kmz --year 2018 --month 6";
+        print $cmd;
 
         $descriptorspec = array(
-        0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
-        1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
-        2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+            0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+            1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+            2 => array("pipe", "w")    // stderr is a pipe that the child will write to
         );
-
 
         $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
 
         if (is_resource($process)) {
 
             while ($s = fgets($pipes[1])) {
-                echo $s;
-
+                print $s;
             }
         }
 
